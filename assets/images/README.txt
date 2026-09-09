230dd94c2b7ecd9a61699405a97ac6e49cf9e6e1
BeccaGu <165323139+BeccaGu@users.noreply.github.com>
2026-09-09 17:02:58 +0100

    Delete assets directory

--- assets/images/README.txt
+++
@@ -1,1 +0,0 @@
-Put GOATFANS photos, case-study images, thumbnails and logo files in this folder.
--- assets/js/site.js
+++
@@ -1,7 +0,0 @@
-
-/*
-GOATFANS quick-edit file.
-Most page copy lives directly in each HTML file.
-Keep shared navigation/footer styling in assets/css/style.css.
-When you have your Apple Music playlist URL, replace APPLE_MUSIC_PLAYLIST_URL in playlist/index.html.
-*/
